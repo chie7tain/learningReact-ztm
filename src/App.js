442d93502f39b01c473f1b683a5623e8eab7d69c
chei7tain <fredrickokwuobi@gmail.com>
2021-12-22 23:06:44 +0100

    change state with value of input tag

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "Hello My guys them",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <input
+            type="text"
+            className="collect"
+            onChange={(e) => this.setState({ string: e.target.value })}
+          />
+          <button
+            onClick={() =>
+              this.setState({
+                string: "wait first make i check something",
+              })
+            }
+          >
+            Change State
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
